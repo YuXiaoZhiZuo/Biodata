23cbf65099bd79a20975d23a7b9fd960cb5677d9
玉箫制作 <282273435@qq.com>
2025-09-14 11:42:33 +0800

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,1 @@
-// 页面加载完成后执行
-document.addEventListener('DOMContentLoaded', function() {
-    // 初始化所有组件
-    initNavigation();
-    initCarousels1();
-    initCarousels2();
-    initRadarCharts();
-});
+document.addEventListener("DOMContentLoaded",(function(){initNavigation(),initCarousels1(),initCarousels2(),initRadarCharts()}));
